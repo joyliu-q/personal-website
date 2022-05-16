ef54ad42a5c47d4ce816a2658611bddae69858c7
Joy Liu <joyliu.q@gmail.com>
2022-05-15 20:05:00 -0700

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,11 @@
+const { web } = require("projen");
+const project = new web.ReactTypeScriptProject({
+  defaultReleaseBranch: "main",
+  name: "personal-website",
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
--- src/components/Footer/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export class Footer extends React.Component{
-    render() {
-        return (
-            <h1>Footer Okay Bye</h1>
-        )
-    };
-}
